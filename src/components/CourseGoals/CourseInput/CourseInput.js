74d4ebb5b25b044163d5266cdf2f0d83de4a7290
Carlos Interone <carlos@mostlyclicks.com>
2021-07-27 06:50:11 -0600

    add dynamic styles in styled component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
+import styled from 'styled-components'
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
+
+
+
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true)
+
+  
 
   const goalInputChangeHandler = event => {
     if (event.target.value.trim().length > 0) {
@@ -26,13 +32,43 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
 
 export default CourseInput;
+
+
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+   
+  & label {
+      font-weight: bold;
+      display: block;
+      margin-bottom: 0.5rem;
+      color: ${props => props.invalid ? "red" : "black"};
+    }
+    
+  & input {
+      display: block;
+      width: 100%;
+      border: 1px solid ${props => props.invalid ? "red" : "gray" };
+      background: ${props => props.invalid ? "rgb(241, 200, 200)" : "transparent"};
+      font: inherit;
+      line-height: 1.5rem;
+      padding: 0 0.25rem;
+    }
+    
+  & input:focus {
+      outline: none;
+      background: #fad0ec;
+      border-color: #8b005d;
+    }
+  
+  `
